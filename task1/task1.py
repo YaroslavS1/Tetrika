4094656b791fe7c39ddfe68b8c6daf484b1a66b6
Iaroslav Suhorukov <yaroslavlol@yandex.ru>
2021-03-18 18:45:07 +0300

    last commit

--- task1/task1.py
+++ task1/task1.py
@@ -1,7 +1,19 @@
 def task(array):
-    '''Сложность O(n)'''
-    return array.find('0')
+    '''Сложность O(2n)'''
+    if isinstance(array, list):
+        data = ''.join(str(i) for i in array)
+    else:
+        data = array
+
+    index = data.find('0')
+    if index == -1:
+        return 'Error! There is no 0 in the array'
+    else:
+        return index
+
 
 
 if __name__ == '__main__':
     print(task("111111111111111111111111100000000"))
+    print(task([1, 1, 1, 1, 0]))
+    print(task([1, 1, 1, 1]))
